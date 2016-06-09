d409e9a3d5016037d1da4fbc8a8c4da0ae8c922e
VegaRuth <ruth222@gmail.com>
2016-06-09 12:49:17 -0500

    Añadiendo background animal print

--- js/main.js
+++ js/main.js
@@ -1,49 +1,14 @@
 $(document).ready (function() {
 console.log("estoy listo");
+/*/font
+$ ( "body" ) .flowtype ({ fontRatio: 30 });
+$ ( "body" ) .flowtype ({ 
+	minimum: 1 , 
+	maximum: 100 , 
+	minFont: 10, 
+	maxFont: 60 ,  
+});*/
 
-/*global jQuery */
-/*!
-* FitText.js 1.2
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
-*
-* Date: Thu May 05 14:23:00 2011 -0600
-*/
-
-	(function( $ ){
-
-	  $.fn.fitText = function( kompressor, options ) {
-
-	    // Setup options
-	    var compressor = kompressor || 1,
-	        settings = $.extend({
-	          'minFontSize' : Number.NEGATIVE_INFINITY,
-	          'maxFontSize' : Number.POSITIVE_INFINITY
-	        }, options);
-
-	    return this.each(function(){
-
-	      // Store the object
-	      var $this = $(this);
-
-	      // Resizer() resizes items based on the object width divided by the compressor * 10
-	      var resizer = function () {
-	        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-	      };
-
-	      // Call once to set.
-	      resizer();
-
-	      // Call on resize. Opera debounces their resize by default.
-	      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-	    });
-
-	  };
-
-	})( jQuery );
 
 	//Ciclo para progress bar
 	for (var i=1; i<=8;i++) {
